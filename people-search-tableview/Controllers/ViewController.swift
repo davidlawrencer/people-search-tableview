81231f28334263efca755333a2e88bf583a7d9cd
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-15 10:20:30 -0400

    Initial commit including tableview controller

---
+++ people-search-tableview/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  people-search-tableview
+//
+//  Created by David Rifkin on 8/15/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
