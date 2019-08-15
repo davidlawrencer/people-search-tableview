4a4efbda9116cf668c7567bd07c4a0b024c7220b
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-15 10:33:22 -0400

    thirds times the charm

---
+++ people-search-tableview/Controllers/PersonalInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PersonalInfoTableViewCell.swift
+//  people-search-tableview
+//
+//  Created by David Rifkin on 8/15/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class PersonalInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
