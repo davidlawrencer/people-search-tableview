3f9ac9fc8a6180ab1a1618f0023cf6bac8e59e54
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-15 15:46:45 -0400

    Adds inheritance comparison example code

---
+++ people-search-tableview/Models/InheritanceComparison.swift
@@ -0,0 +1,38 @@
+//
+//  InheritanceComparison.swift
+//  people-search-tableview
+//
+//  Created by David Rifkin on 8/15/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import Foundation
+/*
+protocol Liveable {
+    var isAlive: Bool {get set}
+}
+
+class Person: Liveable, Equatable {
+    static func == (lhs: Person, rhs: Person) -> Bool {
+        return (lhs.name == rhs.name) && (lhs.isAlive == rhs.isAlive)
+    }
+    
+    var isAlive: Bool
+    var name: String
+    init(name: String, isAlive: Bool ){
+        self.name = name
+        self.isAlive = isAlive
+    }
+}
+
+
+let levi = Person(name: "something", isAlive: true)
+let bee = Person(name: "bee", isAlive: true)
+levi == bee
+
+class Coder: Person {}
+
+let mariel = Coder(name: "mariel", isAlive: true)
+
+mariel == bee
+*/
